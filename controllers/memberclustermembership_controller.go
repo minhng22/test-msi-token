644da8180892270003729425938d70da7a8e8ded
Minh Nguyen <mnguyenn@protonmail.com>
2022-05-10 17:49:39 -0700

    Try removing caCert.

--- controllers/memberclustermembership_controller.go
+++ controllers/memberclustermembership_controller.go
@@ -18,9 +18,6 @@ package controllers
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
-	"net/http"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
@@ -64,7 +61,7 @@ func (r *MemberClusterMembershipReconciler) Reconcile(ctx context.Context, req c
 	if err != nil {
 		return ctrl.Result{}, errors.Wrap(err, "fail getting token")
 	}
-	l.Info("Current access token ", "token", (*t).Token)
+	l.Info("Current access token ", "token", t)
 
 	var demoCluster mngiov1.MemberClusterMembership
 	if err := r.Get(ctx, req.NamespacedName, &demoCluster); err != nil {
@@ -73,20 +70,12 @@ func (r *MemberClusterMembershipReconciler) Reconcile(ctx context.Context, req c
 	l.Info("Creating kubeconfig", "hubServerUrl", hubServerUrl)
 
 	// This ca cert comes from the field certificate-authority-data in the member cluster kubeconfig
-	caCert := []byte("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUZIVENDQXdXZ0F3SUJBZ0lRRCs4SEsrenlVRnkxcVVLczFoUGZpakFOQmdrcWhraUc5dzBCQVFzRkFEQU4KTVFzd0NRWURWUVFERXdKallUQWVGdzB5TWpBMU1UQXdNakV5TXpSYUZ3MHlOREExTVRBd01qSXlNelJhTURBeApGekFWQmdOVkJBb1REbk41YzNSbGJUcHRZWE4wWlhKek1SVXdFd1lEVlFRREV3eHRZWE4wWlhKamJHbGxiblF3CmdnSWlNQTBHQ1NxR1NJYjNEUUVCQVFVQUE0SUNEd0F3Z2dJS0FvSUNBUUN5LzVtdHN0RG9ieFVvQkhnSktzQkMKK1pxOXBhSW53QjhiNlFwbWxTMDVaSngzSTVEcGxNQzlEeGQ0ajk4MlZLWFAwUUdvdUtiNEFHakQ0VkJSTy9pNwp1eXJJRkIwTDRJUFNxVTdaRXBJVW91WmhqcXRUbnJrUDZQcDJxVFgwRTltWDFJaHlpaGprTVB1MDZSSUZtUEVoClFaVzdxRDc4OWhjTVJTN3hjRS9QTEQzMkphYnd4ZURoa08yV2tnMm9lUWtLN0NHRXQxVWNnWFo2aXFWbzJZZnAKWGZCVmlXcnYzT3p3azM1VjhIcnBKTk9LeWo2Qk5qRXFJQ3BEZ1pqWjFBSCtBd1hMNWhtbXJWVTVrTmVyWVJIbwp3OEs2S0wzSkhjTDJiUkdZNzlHeHVjUTJvRkgwRWRkVFJsejNtcUx1Z1dic25jY1M3aGlZNm54V1MwYjByYnFtCkppelYzS0ROaHdTS3Z4a0ZQNEs2LzNHcFNRR2NVbk9EWSswSnIwdDJQZDhxOHluM0xUakx5ckdBOVBKVUpSYWsKTzBuVVRzOStyL2Fzbit4M05kQ3g1bS9JNXZ5clJwekU1aTJ1bXdzenVMZ1NIWUxGOE0rbjErdUJvYnJxb0pTNwptdnovLzBNOEhLKzEwajVyc0ZkdGxDSC9sTDgrcDJWV0JhbEZRTENKL1RQQXlNWlQrQnRMckFaODhsWUFHRnZjCmZTUWNLaVhmVlpwNitBSnd6UVR3T29IQXhkNHliMHRxVVZoR2E4NDFIQVZJU2tjaTJZQW5YWUs2NW93UU1tMDkKVHNHMVdBRUk3Sm4zNmp6SHdCamRieWRycE9VK0plV0ZPQ25NcTREWi9UemJObHhnckR5aHB6RFIwdzlvcU9mSApZd0hvK0pLemQrUWNQYStBejlNV29RSURBUUFCbzFZd1ZEQU9CZ05WSFE4QkFmOEVCQU1DQmFBd0V3WURWUjBsCkJBd3dDZ1lJS3dZQkJRVUhBd0l3REFZRFZSMFRBUUgvQkFJd0FEQWZCZ05WSFNNRUdEQVdnQlN1SldiS1lHY2UKcW8wTFVRY2p4SmJnSkZ2bWZUQU5CZ2txaGtpRzl3MEJBUXNGQUFPQ0FnRUFiUFY3MzdFbWptVjRPUDk0VCtRagp4QmRCZGIwQmRTOXJWT0FEOHd1YXlBUk9hcSt1YTdZUVVXQW5sK2NXVEl5SUFCbTZCS1V2VTJ4RE9sR0RscXdjCkZ2bnh1ZFNBVi9YOTl1QTlaNXZ2ZURBSU1EdlVwR2FQTHRsUmVReTJzM1p6dnoxajZhUGxOS3pwK3A0bkVmK1AKdGlIWWRlTXZHZDVibkM2RkRqS2hjamcxS0lNR2pjWCtiVzNWV29FRURCVmlyVTlDQ3pScXpHUjMxVEphUTRVeApKdmJFR1ErZEl3RWZWNDJ2SFFSblVDU2pzYmhqd0JJNUR3a2lDZ09tRVlzUEh3VDl5R1luYTFjM243WFJHZ3EwCnM1dHlBUWVZZER4V3I5OHQ2dUVJVkd6NEJWTk9VaEs3ZTRXWHJ4TVlPYTU3YWxtWlJiVTJSMnMzaTloK1FOb2YKWGk4MGxFYXBTc2Z3SDVKUnJ6WC9NTFl0b0U4RWx0ZWJxTm9zTml6K3VIRzdtQXdsTUkxR2s3WVVqNnVsYW9DcAo5N1JyczJRNGZnMTc1cGwrVHcvSFMxSUNVeVpuYmdPWUIrMjhnNGRTTmhzVEk3ZWJaald0TUVUMXhJbjVNL0ZuCkZqcE5MMThxckpTWHc4b0lZVExTODBEMzFaOGxhTUZiVG1ybnFGNUttekYxQmhvOWp6VmNNMDZhUHNzb2w2dU4KTVhkdWFPL0liTTJXQ2tJc3B6QXVJeGpWWXl3Q1RKMU13VTYzMDNFS3pEQmJTT1NjbC8wOSthSCs4VW9XMm1wQwpyWkRUWVNBOTNxV1Ywc2s3QWttbHdabjBXclA2ZEQ3cjBoRG1PMnB3Yzg0NFM4UHpYZjJzVTRwb1NmNDJiS1QwCldtQUJGbVMzZEZoYkJKdWxOUHpxOXE4PQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg==")
 	if err != nil {
 		return ctrl.Result{}, errors.Wrap(err, "fail creating cert")
 	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
 	cf := rest.Config{
 		BearerToken: (*t).Token,
 		Host:        hubServerUrl,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
-			},
-		},
 	}
 
 	k8sClient, err := kubernetes.NewForConfig(&cf)
